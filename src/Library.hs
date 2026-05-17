d7c2406ceb8efa6d2c96bba5e716ffdc4a7de189
martinzaj <mzajdenberg@frba.utn.edu.ar>
2026-05-17 15:50:17 -0300

    Parte 1

--- src/Library.hs
+++ src/Library.hs
@@ -18,3 +18,43 @@ data Hamburguesa = Hamburguesa {
     ingredientes :: [Ingrediente]
 } deriving (Eq, Show)
 
+cuartoDeLibra :: Hamburguesa
+cuartoDeLibra = Hamburguesa { precioBase = 20, ingredientes = [Pan, Carne, Cheddar, Pan] }
+
+agrandar :: Hamburguesa -> Hamburguesa
+agrandar unaHamburguesa = agregarIngrediente (ingredienteBase unaHamburguesa) unaHamburguesa
+
+agregarIngrediente :: Ingrediente -> Hamburguesa -> Hamburguesa
+agregarIngrediente unIngrediente unaHamburguesa = unaHamburguesa { ingredientes = unIngrediente : ingredientes unaHamburguesa }
+
+
+
+ingredientesBase :: [Ingrediente]
+ingredientesBase = [Carne, Pollo]
+
+ingredienteBase :: Hamburguesa -> Ingrediente
+ingredienteBase unaHamburguesa = elegirIngredienteBase (filter (estaEn unaHamburguesa) ingredientesBase)
+
+estaEn :: Hamburguesa -> Ingrediente -> Bool
+estaEn unaHamburguesa unIngrediente = elem unIngrediente (ingredientes unaHamburguesa)
+
+elegirIngredienteBase :: [Ingrediente] -> Ingrediente
+elegirIngredienteBase (unIngrediente : _) = unIngrediente
+elegirIngredienteBase [] = Carne
+
+
+
+descuento :: Number -> Hamburguesa -> Hamburguesa
+descuento unPorcentaje unaHamburguesa = unaHamburguesa { precioBase = precioConDescuento unPorcentaje (precioBase unaHamburguesa) }
+
+precioConDescuento :: Number -> Number -> Number
+precioConDescuento unPorcentaje unPrecio = unPrecio * (100 - unPorcentaje) / 100
+
+precioDeIngredientesDeHamburguesa :: Hamburguesa -> Number
+precioDeIngredientesDeHamburguesa = sum . map precioIngrediente . ingredientes
+
+precioFinal :: Hamburguesa -> Number
+precioFinal unaHamburguesa = precioBase unaHamburguesa + precioDeIngredientesDeHamburguesa unaHamburguesa
+
+pdepBurger :: Hamburguesa
+pdepBurger = descuento 20 . agregarIngrediente Cheddar . agregarIngrediente Panceta . agrandar . agrandar $ cuartoDeLibra
